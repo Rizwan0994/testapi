488943f73fbb28cb528890e8a6f09adbc1bb1bcb
Muhammad Rizwan <f190994@nu.edu.pk>
2023-03-30 19:44:56 +0500

    notification controller status check

--- src/controllers/collectorNotificationController.js
+++ src/controllers/collectorNotificationController.js
@@ -1,14 +1,31 @@
 const Notification = require('../models/notificationSchema');
 
+// exports.getNotifications = async (req, res) => {
+//     const collectorId = req.params.collectorId;   
+//     try {
+//         const notifications = await Notification.find({ collectorId, status: null });
+//         res.json(notifications);
+//     } catch (err) {
+//         res.status(500).send(err);
+//     }
+// };
+
 exports.getNotifications = async (req, res) => {
     const collectorId = req.params.collectorId;   
     try {
+        // check if the status field exists for the collectorId
+        const existingNotifications = await Notification.findOne({ collectorId, status: { $exists: true } });
+        if (!existingNotifications) {
+            // if the status field does not exist, set it to null by default
+            await Notification.updateMany({ collectorId }, { $set: { status: null } });
+        }
         const notifications = await Notification.find({ collectorId, status: null });
         res.json(notifications);
     } catch (err) {
         res.status(500).send(err);
     }
 };
+
 
 exports.updateNotificationStatus = async (req, res) => {
     const notificationId = req.params.notificationId;
