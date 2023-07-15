2fe0e40401e82c6055f0a845476871248d6a8f3e
Rizwan0994 <f190994@nu.edu.pk>
2023-07-15 07:25:21 +0500

    schedule status set

--- src/models/schedule.js
+++ src/models/schedule.js
@@ -1,24 +1,33 @@
-const mongoose =require("mongoose");
-const UserSchema=mongoose.Schema({
-     nearestYard:{
-        type:String,
-        required:true
-     },
-     sDate:{
-        type:String,
-        required:true
-     },
-     sTime:{
-        type:String,
-        required:true
-     },  
-     itemDetails:{
-        type:String,
-        required:true
-     },  
-     userId:{
-        type:String,
-        required:true
-     }
-},{timestamps:true});
-module.exports=mongoose.model("PickupSchedule", UserSchema);
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+  {
+    nearestYard: {
+      type: String,
+      required: true,
+    },
+    sDate: {
+      type: String,
+      required: true,
+    },
+    sTime: {
+      type: String,
+      required: true,
+    },
+    itemDetails: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("PickupSchedule", UserSchema);
